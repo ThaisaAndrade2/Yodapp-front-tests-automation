8317749fe65c5d7bd05ff58931b8d85a675ee266
Thaisa Andrade <thaisasantosandrade7@gmail.com>
2022-01-20 20:12:07 -0300

    feat: create user factory

---
+++ resources/factories/users.py
@@ -0,0 +1,26 @@
+
+def factory_yoda():
+
+    user = {
+        "name": "Mestre Yoda",
+        "email": "yoda@jedi.com",
+        "order": "Jedi",
+        "tpjedi": "Cavaleiro Jedi",
+        "bdate":  "fevereiro-1999-22",
+        "insta": "@yoda"
+    }
+
+    return user
+
+
+def factory_darth_vader():
+
+    user = {
+        "name": "Darth Vader",
+        "email": "vader&hotmail.com",
+        "order": "Sith",
+        "bdate":  "fevereiro-2001-8",
+        "insta": "@vader"
+    }
+
+    return user
